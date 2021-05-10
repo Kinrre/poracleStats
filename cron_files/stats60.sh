7d967ad8ae70a9a2435cfbae39bebf12386e4a38
dkmur <42545952+dkmur@users.noreply.github.com>
2021-05-10 08:24:33 +0200

    Update stats60.sh

--- cron_files/stats60.sh
+++ cron_files/stats60.sh
@@ -10,6 +10,7 @@ interval=$(date -d '1 hour ago' +%Y"-"%m"-"%d" "%H)
 interval_middleman=$(date -d '1 hour ago' +%d"/"%b"/"%Y" "%H)
 
 ## Get all poracle logs
+echo ""
 echo "Get all poracle logs"
 echo ""
 mkdir -p $folder/tmp
@@ -21,7 +22,6 @@ grep "$interval" $PATH_TO_PoraclJS/logs/general-$process_date.log > $folder/tmp/
 
 ## Get controller log data
 echo "grep controller log data"
-echo ""
 Umon="$(grep :user $folder/tmp/controller.log | grep 'Creating monster alert' | wc -l)"
 Uraid="$(grep :user $folder/tmp/controller.log | grep 'Creating raid alert' | grep Creating | wc -l)"
 Uegg="$(grep :user $folder/tmp/controller.log | grep 'Creating egg alert' | grep Creating | wc -l)"
@@ -55,7 +55,6 @@ fi
 
 ## Get error log  data
 echo "grep error log data"
-echo ""
 warn="$(grep 'MAIN warn' $folder/tmp/errors.log | wc -l)"
 warnMap="$(grep 'MAIN warn' $folder/tmp/errors.log | grep StaticMap | wc -l)"
 warnRL="$(grep 'MAIN warn' $folder/tmp/errors.log | grep 'rate limit hit' | wc -l)"
@@ -72,7 +71,6 @@ fi
 
 ## Get discord log  data
 echo "grep discord log  data"
-echo ""
 warn2="$(grep 'MAIN warn' $folder/tmp/discord.log | wc -l)"
 error2="$(grep 'MAIN error' $folder/tmp/discord.log | wc -l)"
 errorBG="$(grep 'MAIN error' $folder/tmp/discord.log | grep 'Bad Gateway' | wc -l)"
@@ -92,7 +90,6 @@ fi
 
 ## Get general log  data
 echo "grep general log  data"
-echo ""
 whQinMin="$(grep '$STATS info: Worker STATS: WebhookQueue is currently' $folder/tmp/general.log | awk '{print ($NF)}' | jq -s min)"
 whQinMax="$(grep '$STATS info: Worker STATS: WebhookQueue is currently' $folder/tmp/general.log | awk '{print ($NF)}' | jq -s max)"
 whQinAvg="$(grep '$STATS info: Worker STATS: WebhookQueue is currently' $folder/tmp/general.log | awk '{print ($NF)}' | jq -s add/length)"
@@ -113,13 +110,12 @@ fi
 
 ## Check for middleman path and process
 echo "Middleman data"
-echo ""
 if [ -z "$PATH_TO_middleman_log" ]
 then
   echo "No path entered, skipping"
 else
   echo "Get log data"
-  grep "$interval_middleman" $PATH_TO_middleman_log/middleman-error.log > $folder/tmp/middleman.log
+  grep -a "$interval_middleman" $PATH_TO_middleman_log/middleman-error.log > $folder/tmp/middleman.log
   echo "grep middleman log data"
   mm200="$(grep 'POST /staticmap' $folder/tmp/general.log | grep '200' | wc -l)"
   mm500="$(grep 'POST /staticmap' $folder/tmp/general.log | grep '500' | wc -l)"
